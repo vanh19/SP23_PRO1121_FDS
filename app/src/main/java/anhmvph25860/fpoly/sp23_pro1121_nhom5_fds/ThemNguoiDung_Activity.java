3d4d9d75137b1d73c2ecedce92d40abc740543cb
Vietanh-19 <https://github.com/Vietanh-19/test_git.git>
2023-03-23 01:13:59 +0700

    .

---
+++ app/src/main/java/anhmvph25860/fpoly/sp23_pro1121_nhom5_fds/ThemNguoiDung_Activity.java
@@ -0,0 +1,14 @@
+package anhmvph25860.fpoly.sp23_pro1121_nhom5_fds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemNguoiDung_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_nguoi_dung);
+    }
+}
