a0996ca5d08ee3385c04327736f581730768b007
Vietanh-19 <https://github.com/Vietanh-19/test_git.git>
2023-03-23 00:09:02 +0700

    Initial commit

---
+++ app/src/test/java/anhmvph25860/fpoly/sp23_pro1121_nhom5_fds/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package anhmvph25860.fpoly.sp23_pro1121_nhom5_fds;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
